acae219b662112e79d0a684aaab5029ef14b5b13
Xd_Golden_Tiger <xdgoldentigerbusiness@gmail.com>
2023-12-09 14:43:01 +1300

    Updates

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\TBNR_\OneDrive\Desktop\vMenuDocs\venv
+set VIRTUAL_ENV=E:\Golden Development\Pre-made Files\FiveM\vMenu Revamped Docs\vMenuDocs\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
